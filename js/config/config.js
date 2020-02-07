19f7cc10d875ec2ffe3d332c11b280de9a17dc9c
Florian Jenett <fjenett@gmail.com>
2020-02-07 16:50:53 +0100

    fixing config

---
+++ js/config/config.js
@@ -0,0 +1,10 @@
+var config = {
+  piecemaker : {
+    host : 'http://api.piecemaker.org'
+  },
+  media : {
+    base_url : '/pm2-public/piecemaker',
+    host : 'd35vpnmjdsiejq.cloudfront.net',
+    streamer : 's12vlv7g59gljg.cloudfront.net'
+  }
+}
